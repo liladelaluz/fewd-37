d8440fb29fb16ed13ae63b6ed73884846711fe66
liladelaluz <liladelaluz@yahoo.com>
2016-01-06 18:24:11 -0800

    Work in progress
    Created a stop light.

---
+++ stopLight.js
@@ -0,0 +1,45 @@
+
+var stopButton = document.querySelector(".stopButton");
+var slowButton = document.querySelector(".slowButton");
+var goButton = document.querySelector(".goButton");
+
+
+
+stopButton.addEventListener("click", fstopButton);
+slowButton.addEventListener("click", fslowButton);
+goButton.addEventListener("click", fgoButton);
+
+
+var stopLight = document.querySelector("#stopLight");
+var yieldLight = document.querySelector("#yieldLight");
+var goLight = document.querySelector("#goLight");
+
+
+
+function stopButton(event) {
+    alert("this is good");
+}
+
+function yieldLight(event) {
+  console.log("This works.");
+}
+// var light = document.querySelector(.light);
+// stopLight appendChild(light)
+
+
+//
+// fixture appendChild(#stopLight)
+//
+//
+// fixture appendChild(.slowLight)
+//
+// fixture appendChild(.goLight);
+
+
+/*
+var aKey = document.querySelector("#A");
+aKey.addEventListener("click", playA);
+function playA(event) {
+  howl.play("A")
+}
+*/
