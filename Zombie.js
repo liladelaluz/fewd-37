d06250f9012ceb988ca21a69aad7a0e09815be92
liladelaluz <liladelaluz@yahoo.com>
2016-01-25 17:54:54 -0800

    Zombie is running great.
    Need to add a silence element for the audio to stop when the button is
    hit.

--- Zombie.js
+++ Zombie.js
@@ -6,13 +6,13 @@ var Q = [
   "Today, be the observer. Keep your eyes present to what is and how it operates in the space that surronds you.",
   "Practice active listening. Take in what is being said and let it marinate.",
   "You are The Vehicle of Change. Naviage with care and self preservation, to the destinations you desire.",
-  "If you love and respect yourself first, you can easily love and respect others. Treat yourself to an awesome gift today, by doing something for you.",
+  "If you love and respect yourself first, you can easily love and respect others. Treat yourself to an awesome gift today! You are worthy.",
   "Engage in uplifting activities today. Think about what will lift your spirits even higher today and do that. Set a goal that is attainable.",
   "Checkin with your body today. Dive in deeper to listening to what it desires. Do you require more rest? Do you crave ice cream, but what you really need is calcium? Find the root of your body's needs and fill them.",
-  "Take 10! Take 10 minutes to close your eyes, focus on your breath and with each breath, try to breath deeper. You can do this anywhere, so get it done, my friend!",
+  "Take 10! Take 10 minutes to close your eyes, focus on your breath and with each breath, try to breath deeper. You can do this anywhere, so get it done, my friend! Breath deep.",
   "Activate Super Powers in the form of__________! What super hero(ine) would you like to be today? Make one gesture that reflects your powers.",
   "Build the world you dream to live in! Complete one call to action. If you desire to build sustainable environments, make a point to buy local today. If you desire to save the condors, donate your time or money. You get the idea... take action.",
-  "Something goes here!",
+  "Be fearless today! Try whatever you desire without attachment to a final result. As long as you put your best foot forward and try, you have succeeded, what happens next is a just a lesson for your personal-play-book.",
 ];
 
 
@@ -52,70 +52,64 @@ var images = [
        "newImages/image_26.jpg",
        "newImages/image_27.jpg",
        "newImages/image_28.jpg",
-       "newImages/image_29.jpg",
-       "newImages/image_30.jpg",
 ]
 
-// var container = document.createElement("section");
-// container.setAttribute("class", "container");
-// body.appendChild(container);
 
 var button = document.querySelector("button");
 button.addEventListener("click", function () {
+  // p is set to hidden to clear any quotes on the page
   p.classList.add('hidden');
   button.classList.add('hidden');
   document.querySelector('header').classList.add('hidden');
   var footer = document.querySelector("footer");
   footer.classList.add('hidden');
+  var audio = document.querySelector("audio");
+  audio.classList.add('hidden');
   backgroundCounter = 0;
-  interval = setInterval(setBackgroundImage, 100);
+  interval = setInterval(setBackgroundImage, 400);
 });
-// body.appendChild(button);
+
 
 var p = document.createElement("p");
 p.setAttribute("class", "quote");
 body.appendChild(p);
 
+
 function randomQuote() {
+  // JSON.parse parses a string // returns an integer
   var quotes = JSON.parse(localStorage.getItem('quotes'));
+// math.floor returns an integer // math.random creates a random number between 0-1
   var randomNumber = Math.floor(Math.random() * (quotes.length-1));
   var quote = quotes[randomNumber];
 
-  // take the random number and based on that index, assign it to a p tag and append p tag to the container
+  // take the random number and based on that index, assign it to a p tag and append p tag to the container and splices
+  // splice removes 1 element from randomNumber
   quotes.splice(randomNumber, 1);
   p.textContent = quote;
   if (quotes.length === 0) {
     quotes = Q;
   }
-
   localStorage.setItem('quotes', JSON.stringify(quotes));
   }
-
-//
-// var unseenQuotes = document.createElement("p").setAttribute("class", "unSeenQuotes");
-//
-// localStorage.setItem("unseenQuotes");
-// localStorageQuote = localStorage.getItem("unseenQuotes", "p");
-// // attaching quote to container
-// var p = document.createElement("p");
-// p.textContent = unSeenQuotes;
-// container.appendChild(p);
-//
-
-var backgroundCounter = 0;
-var backgroundMax = 13;
-var interval;
-function setBackgroundImage () {
-  if (backgroundCounter >= backgroundMax) {
-    clearInterval(interval);
-    randomQuote();
-    p.classList.remove('hidden');
-    button.classList.remove('hidden');
-    // showQuote();
-    return;
-  }
+  // backgroundImage cycles through 8 times and returns a random quote
+  var backgroundCounter = 0;
+  var backgroundMax = 8;
+  var interval;
+  function setBackgroundImage () {
+    if (backgroundCounter >= backgroundMax) {
+      // clearInterval will stop the time after the backgroundMax of 8 cycled through
+      clearInterval(interval);
+      randomQuote();
+      // p was hidden whith the button eventListener function
+      p.classList.remove('hidden');
+      // the button appears again
+      button.classList.remove('hidden');
+      // showQuote();
+      return;
+    }
 
 
+// generate a random number for backgroundImages Math.floor rounds down
   var randomNumber = Math.floor(Math.random() * (images.length-1));
   document.body.style.backgroundImage = "url('" + images[randomNumber] + "')";
   backgroundCounter++;
@@ -125,48 +119,24 @@ setTimeout(function () {
   document.querySelector('.dancingMuerto').classList.toggle('hidden');
   body.classList.toggle('hidden');
 }, 7000)
-//var interval = setInterval(setBackgroundImage, 300);
-// setTimeout(function () {
-//   document.querySelector('#skullGif').classList.toggle('hidden');
-//   body.classList.toggle('hidden');
-// }, 700)
 
 
 
-// if (background-Image.length === 0){
-//   background-Image = randomImage
-//
-//   var
+
+
 //   // reset backround-Image
-// }
-// get a random quote
-  // var i = 0
+
   // Q[i]i++ Math.random() Q.length
 //   for (var i = 0; i < Math.random()Q.length; i++) {
 //     Math.random()Q[i]
 //   }
-//   // localStorage.setItem("quotes", v)
-//   jason.stringify(Q)"..."
 //
-//   // localStorageGetItem("quotes")
-//   jason.parse(s)
+//
+//
 // // delete what has already been seen in local storage
 //   q.splice(i,j)delete
-// // change body background color
-// // change container background color
-// // Create Div Element
-  // Log In Name Label and log in name input from Facebook
-  // Log in password label and log in password input from Facebook
-  // (these buttons are to collect the history of the user and to always provide them with a new mantra, once they use all the mantras, the program will reset)
-  // Submit button to pass in these buttons
+//
 
-
-
-// var prompt = document.createElement("popOut");
-// popOut.setAttribute("id", "prompt");
-// popOut.textContent = "Log In Name", "Log In Password");
 
 
 // the loop will run through to the length of the quote by visiting each index of the array
-// var randomNum = getRandom(localStorageQuote);
-// var quote = localStorageQuote[randomNum];
